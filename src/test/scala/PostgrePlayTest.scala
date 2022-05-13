346ceccb29d709b81aeda42a6b3171e15240c877
andri <zuliyandri@gmail.com>
2022-05-14 01:23:57 +0700

    modify

--- src/test/scala/PostgrePlayTest.scala
+++ src/test/scala/PostgrePlayTest.scala
@@ -1,41 +1,40 @@
-package zul.province.app
-
+import models.{Province, ProvinceDAO}
 import org.scalatestplus.play.PlaySpec
-import play.api.db.Databases
-import slick.lifted.{Executable, TableQuery}
-import zio.{IO, Runtime, UIO, ZIO, console}
-import zul.province.app.models.Provinces
-import zul.province.app.models.Provinces.{Province, ProvincesModel}
-import zul.province.app.services.ProvincesServices.ProvinciesServices
-import ZIO._
 
 import java.sql.Date
-import zio.console._
+import scala.util.{Failure, Success}
 
 class PostgrePlayTest extends PlaySpec {
 
-  /*"Connection Database" must {
-    "should be conect" in {
-      val db = Databases(
-        driver = "org.postgresql.Driver",
-        url = "jdbc:postgresql://localhost:5432/provinces",
-        config = Map(
-          "username" -> "postgres",
-          "password" -> "root"
-        )
-      )
-      val connection = db.getConnection()
-      println(connection.getClientInfo)
-      db.shutdown()
+  /*"delete data" must {
+    "data should be empty" in {
+      import services.Executor._
+      ProvinceDAO.delete(List(1,2,3,4,5))
     }
   }*/
 
-  "Insrt data" must {
-    "should be insert" in {
-      /*val p = Province(Some(2), "Zuliandri", "Hello Zuliandri", created = Some(new Date(System.currentTimeMillis())))
-      ProvinciesServices.create(Provinces.insert(p))*/
-      val suk = Runtime.default.unsafeRun(ZIO.succeed(42))
-      println(suk)
+  "insert data" must {
+    "data should'nt be empty" in {
+      import services.Executor._
+      val p = Province(Some(1), "Zuliandri", "Welcome Zuliandri", created = Some(new Date(System.currentTimeMillis())))
+      ProvinceDAO.insert(p).onComplete {
+        case Success(value) => println("success")
+        case Failure(exception) => exception.printStackTrace()
+      }
+      ProvinceDAO.insert(p.copy(Some(2), "Andri", "Welcome Andri", created = Some(new Date(System.currentTimeMillis()))))onComplete {
+        case Success(value) => println("success 1")
+        case Failure(exception) => exception.printStackTrace()
+      }
+    }
+  }
+
+  "soft delete data" must {
+    "data should be updated" in {
+      import services.Executor._
+      ProvinceDAO.softDelete(List(2))onComplete {
+        case Success(value) => println("success 3")
+        case Failure(exception) => exception.printStackTrace()
+      }
     }
   }
 
