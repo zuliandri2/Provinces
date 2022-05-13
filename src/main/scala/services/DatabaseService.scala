a01367a615f44b91358b0a3aab18ed2f165ea1b4
andri <zuliyandri@gmail.com>
2022-05-14 01:21:57 +0700

    modify

--- src/main/scala/services/DatabaseService.scala
+++ src/main/scala/services/DatabaseService.scala
@@ -1,24 +1,15 @@
-package zul.province.app
 package services
 
-import akka.actor.ActorSystem
-import play.api.db.slick.HasDatabaseConfig
-import play.api.libs.concurrent.CustomExecutionContext
-import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
+object DatabaseService {
 
-object DatabaseService extends HasDatabaseConfig[JdbcProfile] {
-  override protected val dbConfig = DatabaseConfig.forConfig("slick.dbs.default")
-
-  def exec[T](block: DatabaseConfig[JdbcProfile] => T) = {
-    block(dbConfig)
+  def exec[T](block: JdbcProfile#Backend#Database => T) = {
+    try {
+//      block(db)
+    } catch {
+      case e: Exception => e.printStackTrace
+    }
   }
+
 }
-
-trait DatabaseExecutionContext extends ExecutionContext
-
-@Singleton
-class DatabaseExecutionContextImp @Inject() ( system: ActorSystem ) extends CustomExecutionContext ( system, "database.dispatcher") with DatabaseExecutionContext
