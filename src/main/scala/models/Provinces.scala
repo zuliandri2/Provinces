43f0dc94902aba23835ce6a782f27ed33717d205
andri <zuliyandri@gmail.com>
2022-05-14 01:26:17 +0700

    modify

--- src/main/scala/models/Provinces.scala
+++ src/main/scala/models/Provinces.scala
@@ -1,17 +1,29 @@
-package zul.province.app
 package models
 
-import slick.jdbc.GetResult
-import slick.jdbc.H2Profile.api._
-import java.sql.Date
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfig, HasDatabaseConfigProvider, NamedDatabaseConfigProvider}
+import slick.jdbc.{GetResult, JdbcProfile}
 
-object Provinces {
+import java.sql.Date
+import slick.basic.{BasicProfile, DatabaseConfig}
+
+import scala.concurrent.Future
+
+trait ProvinceProfile {
+//  val profile: JdbcProfile
+}
+
+trait ProvinceDatabase extends HasDatabaseConfig[JdbcProfile] with ProvinceProfile {
+  override protected val dbConfig = DatabaseConfig.forConfig("slick.dbs.default")
+}
+
+case class Province(id: Option[Int], name: String, description: String, updated: Option[Date] = None, deleted: Option[Date] = None, created: Option[Date] = None)
+
+trait ProvinceComponent extends ProvinceDatabase {
+  import profile.api._
 
   implicit val getProvincesResult = GetResult(r => Province(Some(r.nextInt()), r.nextString(), r.nextString(), r.nextDateOption(), r.nextDateOption(), r.nextDateOption()))
 
-  case class Province(id: Option[Int], name: String, description: String, updated: Option[Date] = None, deleted: Option[Date] = None, created: Option[Date] = None)
-
-  class ProvincesModel(tag: Tag) extends Table[Province](tag, "provinces") {
+  class ProvinceTable(tag: Tag) extends Table[Province](tag, "provinces") {
     def id = column[Int]("id", O.PrimaryKey, O.Unique, O.SqlType("SERIAL"))
     def name = column[String]("name")
     def description = column[String]("description", O.Default(""))
@@ -21,8 +33,44 @@ object Provinces {
     def * = (id.?, name, description, updated, deleted, created).mapTo[Province]
   }
 
-  private lazy val provincesTable = TableQuery[ProvincesModel]
+  protected lazy val provinceTableQuery = TableQuery[ProvinceTable]
 
-  def insert(p: Province) = provincesTable += p
+}
+
+object ProvinceDAO extends ProvinceComponent {
+
+  import profile.api._
+  import services.Executor._
+
+  def insert(province: Province) = for {
+      a <- Future { province.copy(created = Some(new Date(System.currentTimeMillis()))) }
+      p <- Future { provinceTableQuery += a }
+      u <- db.run(p)
+    } yield u
+
+  def findById(id: Int) = for {
+    f <- Future { provinceTableQuery.filter(p => p.id === id).result }
+    l <- db.run(f)
+  } yield l
+
+  def findById(id: List[Int]) = for {
+    f <- Future { provinceTableQuery.filter(p => p.id inSet id ).result }
+    l <- db.run(f)
+  } yield l
+
+  def delete(id: Int) = for {
+    f <- Future { provinceTableQuery.filter(p => p.id === id).delete }
+    r <- db.run(f)
+  } yield r
+
+  def delete(id: List[Int]) = for {
+    f <- Future { provinceTableQuery.filter(p => p.id inSet id).delete }
+    r <- db.run(f)
+  } yield r
+
+  def softDelete(id: List[Int]) = for {
+    f <- Future { provinceTableQuery.filter(p => p.id inSet id).map(p => (p.deleted)).update(Some(new Date(System.currentTimeMillis()))) }
+    r <- db.run(f)
+  } yield r
 
 }
